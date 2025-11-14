fcdc4adbec1c80c84b6c71750454381775f0f68d
Paulo Borges <poolborges@hotmail.com>
2025-11-14 23:01:40 +0000

    Add Stock transaction DTO

---
+++ kriolos-opos-domain/src/main/java/com/openbravo/pos/inventory/ProductStockTransaction.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2025 Paulo Borges
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.openbravo.pos.inventory;
+
+import java.util.Date;
+
+/**
+ *
+ * @author psb
+ */
+public class ProductStockTransaction {
+    private String id;
+    private Date transactionDate;
+    private int reasonId;
+    private String locationId;
+    private String productId;
+    private String productAttribSetId;
+    private Double units;
+    private Double price;
+    private String userId;
+    private String supplierId;
+    private String supplierDoc;
+    
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public int getReasonId() {
+        return reasonId;
+    }
+
+    public void setReasonId(int reasonId) {
+        this.reasonId = reasonId;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductAttribSetId() {
+        return productAttribSetId;
+    }
+
+    public void setProductAttribSetId(String productAttribSetId) {
+        this.productAttribSetId = productAttribSetId;
+    }
+
+    public Double getUnits() {
+        return units;
+    }
+
+    public void setUnits(Double units) {
+        this.units = units;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSupplierDoc() {
+        return supplierDoc;
+    }
+
+    public void setSupplierDoc(String supplierDoc) {
+        this.supplierDoc = supplierDoc;
+    }
+    
+    
+}
