cdd6dc00e938a43af6609f666f319431eb7531bb
Paulo Borges <poolborges@hotmail.com>
2025-11-19 00:18:14 +0000

    ::Domain Added package-info

---
+++ kriolos-opos-domain/src/main/java/com/openbravo/pos/epm/package-info.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2022 KriolOS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Employee Presence Management (EPM) Module
+ * 
+ * <p>
+ * A module for managing employee performance through break, leaves.
+ * </p>
+ * 
+ * <ul>
+ *  <li>
+ *      <b>Breaks:</b> This typically refers to short, authorized periods during the workday for rest, meals, or other breaks. 
+ * EPM systems track these to ensure labor law compliance and accuracy in payroll processing
+ *  </li>
+ *  <li>
+ *      <b>Leave:</b> This refers to planned and approved absences from work, such as sick days, vacation, 
+ * or personal time off, and is managed through a company's specific leave policies.
+ *  </li>
+ * </ul>
+ *
+ * @since 1.0
+ * @author poolborges
+ * @version 1.0
+ */
+package com.openbravo.pos.epm;
